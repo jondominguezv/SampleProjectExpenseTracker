79bc917e6bded027e3fe37fa2636a7f1dfc4ff4d
jondominguezv <55215363+jondominguezv@users.noreply.github.com>
2025-08-08 17:30:29 -0400

    Add project files.

---
+++ PersonalExpenseTrackerWeb/Models/TransactionModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalExpenseTrackerWeb.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public CategoryModel Category { get; set; }
+
+        public decimal Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
---
+++ PersonalExpenseTrackerWeb/Models/TransactionsDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalExpenseTrackerWeb.Models
+{
+    public class TransactionsDBContext:DbContext
+    {
+        public TransactionsDBContext(DbContextOptions options):base(options)
+        { }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+}
